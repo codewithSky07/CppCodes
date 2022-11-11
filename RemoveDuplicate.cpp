46db06263a0625f9ff7097710a73c0348041cf76
Code with Sky <115218945+codewithSky07@users.noreply.github.com>
2022-11-11 13:53:08 +0530

    Create RemoveDuplicate.cpp

---
+++ RemoveDuplicate.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+
+        int insertIndex = 1;
+
+        for(int i = 1; i < nums.size(); i++){
+    // We skip to next index if we see a duplicate element
+            if(nums[i - 1] != nums[i]) {
+    // Storing the unique element at insertIndex index and incrementing the insertIndex by 1
+                nums[insertIndex] = nums[i];
+                insertIndex++;
+            }
+        }
+        return insertIndex;
+    }
+};
