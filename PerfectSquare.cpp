610e2b988a2df43f7c9c0892ec7113809edcc855
Code with Sky <115218945+codewithSky07@users.noreply.github.com>
2022-11-22 09:06:58 +0530

    Create PerfectSquare.cpp

---
+++ PerfectSquare.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int numSquares(int n) {
+        vector<int>dp(n+1);
+        
+        dp[0] = 0;
+        dp[1] = 1;
+        
+        for(int i=2;i<=n;i++){
+            int min = INT_MAX;
+            for(int j=1;j*j<=i;j++){
+                
+                int rem = i-j*j;
+                if(dp[rem]<min){
+                    min = dp[rem];
+                }
+                
+            }
+            dp[i] = min+1;
+        }
+        return dp[n];
+    }
+};
