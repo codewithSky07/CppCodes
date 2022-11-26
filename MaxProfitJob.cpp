591a13bdef7b4c068bf720f117faaaeaee28e9f4
Code with Sky <115218945+codewithSky07@users.noreply.github.com>
2022-11-26 12:37:24 +0530

    Create MaxProfitJob.cpp

---
+++ MaxProfitJob.cpp
@@ -0,0 +1,56 @@
+class Solution {
+public:
+    //binary search  to find latest non overlapping interval
+    int latest_non_ov_opt(vector<vector<int>> &v,int i)
+    {
+        int low = 0, high = i - 1; 
+        while (low <= high) 
+        { 
+            int mid = (low + high) / 2; 
+            if (v[mid][0] <= v[i][1])  // end time is less then start time of current job.
+            { 
+                if (v[mid + 1][0] <= v[i][1]) // increment the low. normal binary search.
+                    low = mid + 1; 
+                else                // return lower bound.
+                    return mid; 
+            } 
+            else
+                high = mid - 1; 
+        } 
+        return -1; 
+    }
+    
+    
+    
+    int jobScheduling(vector<int>& startTime, vector<int>& endTime, vector<int>& profit) {
+        int n = startTime.size();
+        
+        vector<vector<int>>v(n,vector<int>(3));
+        
+        for(int i=0;i<n;i++)
+        {
+            v[i] = {endTime[i],startTime[i],profit[i]};
+        }
+            
+//             Sort it according to endtime.
+        sort(v.begin(),v.end());
+        
+//         To store the profit upto that job.
+        vector<int> dp(n,0);
+        dp[0] = v[0][2];
+        
+        
+        for(int i=1;i<n;i++)
+        {
+           
+            int exclusive = dp[i-1];
+            int inclusive = v[i][2]; // profit of new job to be included if we do job
+            int index = latest_non_ov_opt(v,i);
+            // prev job whose end time was smaller then current job start time.
+            if(index!=-1)
+                inclusive += dp[index]; //. profit that is appearing before start time of this job.
+            dp[i] = max(inclusive,exclusive);
+        }
+        return dp[n-1];
+    }
+};
