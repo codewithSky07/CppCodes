5bfc3ad1722f8f55f46babd120436a898dc6c809
Code with Sky <115218945+codewithSky07@users.noreply.github.com>
2022-11-26 12:43:43 +0530

    Update MaxProfitJob.cpp

--- MaxProfitJob.cpp
+++ MaxProfitJob.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
     //binary search  to find latest non overlapping interval
-    int latest_non_ov_opt(vector<vector<int>> &v,int i)
+    int latest_non_overlap_opt(vector<vector<int>> &job,int i)
     {
         int low = 0, high = i - 1; 
         while (low <= high) 
         { 
             int mid = (low + high) / 2; 
-            if (v[mid][0] <= v[i][1])  // end time is less then start time of current job.
+            if (job[mid][0] <= job[i][1])  // end time is less then start time of current job.
             { 
-                if (v[mid + 1][0] <= v[i][1]) // increment the low. normal binary search.
+                if (job[mid + 1][0] <= job[i][1]) // increment the low. normal binary search.
                     low = mid + 1; 
                 else                // return lower bound.
                     return mid; 
@@ -25,27 +25,27 @@ public:
     int jobScheduling(vector<int>& startTime, vector<int>& endTime, vector<int>& profit) {
         int n = startTime.size();
         
-        vector<vector<int>>v(n,vector<int>(3));
+        vector<vector<int>>job(n,vector<int>(3));
         
         for(int i=0;i<n;i++)
         {
-            v[i] = {endTime[i],startTime[i],profit[i]};
+            job[i] = {endTime[i],startTime[i],profit[i]};
         }
             
 //             Sort it according to endtime.
-        sort(v.begin(),v.end());
+        sort(job.begin(),job.end());
         
 //         To store the profit upto that job.
         vector<int> dp(n,0);
-        dp[0] = v[0][2];
+        dp[0] = job[0][2];
         
         
         for(int i=1;i<n;i++)
         {
            
             int exclusive = dp[i-1];
-            int inclusive = v[i][2]; // profit of new job to be included if we do job
-            int index = latest_non_ov_opt(v,i);
+            int inclusive = job[i][2]; // profit of new job to be included if we do job
+            int index = latest_non_overlap_opt(job,i);
             // prev job whose end time was smaller then current job start time.
             if(index!=-1)
                 inclusive += dp[index]; //. profit that is appearing before start time of this job.
