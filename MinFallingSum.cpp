74db2a34d3b2814518265fc7aa740c771eb621f3
Code with Sky <115218945+codewithSky07@users.noreply.github.com>
2022-12-13 10:28:44 +0530

    Update MinFallingSum.cpp

--- MinFallingSum.cpp
+++ MinFallingSum.cpp
@@ -1,20 +1,17 @@
 class Solution {
 public:
-    int minFallingPathSum(vector<vector<int>>& matrix) {
-        
-        int n = matrix.size();
+ int minFallingPathSum(vector<vector<int>>& matrix) {
+ int n = matrix.size();
         int m = matrix[0].size();
-        
+// If only one row. return minimum ..
         if(n == 1) return *min_element(matrix[0].begin(),matrix[0].end());
-        
-        for(int i=0;i<n;i++){
+
+// Otherwise traverse the entire matrix and update it.
+        for(int i=1;i<n;i++){
             for(int j=0;j<m;j++){
-                if(i == 0){
-                    matrix[i][j] = matrix[i][j];
-                }else if(i>0){
                     if(j == 0){
                         matrix[i][j] = matrix[i][j]+min(matrix[i-1][j],matrix[i-1][j+1]);
-                        
+
                     }else if(j == m-1){
                         matrix[i][j] = matrix[i][j]+min(matrix[i-1][j],matrix[i-1][j-1]);
                     }else{
@@ -22,10 +19,9 @@ public:
                     }
                 }
             }
-        }
-        
+
+// Return the minimum out of last elements.
         return *min_element(matrix[n-1].begin(),matrix[n-1].end());
-        
-        return 0;
+
     }
 };
