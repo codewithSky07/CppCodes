7d8d8c8c6a723634acff22164ac3a384111d9602
Code with Sky <115218945+codewithSky07@users.noreply.github.com>
2022-12-25 12:10:02 +0530

    Update LongestSubsequenceWithLimitedSum.cpp

--- LongestSubsequenceWithLimitedSum.cpp
+++ LongestSubsequenceWithLimitedSum.cpp
@@ -6,26 +6,22 @@ public:
         int m = queries.size();
         
         sort(nums.begin(),nums.end());
-        int sum = 0;
-        vector<int> prefixsum;
         
-        for(int i=0;i<n;i++)
+        vector<int>prefixsum(n);
+
+        int temp=0;
+        for(int i =0;i<n;i++)
         {
-            sum += nums[i];
-            prefixsum.push_back(sum);           
+           temp +=nums[i];
+           prefixsum[i] = temp;
         }
-        
-        vector<int> ans(m,0);
+        vector<int>ans;
         for(int i=0;i<m;i++)
         {
-            for(int j=0;j<n;j++)
-            {
-                  // wherever we got the max subsequence sum for i just update that
-                if(prefixsum[j] <= queries[i]) 
-                    ans[i] = j+1;        
-                else break;
-            }
+            int query = queries[i];
+             ans.push_back(upper_bound(prefixsum.begin(),prefixsum.end(),query)-prefixsum.begin());           
         }
+        
         return ans;
     }
 };
